cec8aeeeeb4e4689661a3ff005e7d419e5baba83
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-24 11:13:45 -0500

    setup initial folders and created store in index.js

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  isLoading: false,
+  isAdding: false,
+  isEditing: false,
+  data: []
+};
+
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
